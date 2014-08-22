beccfb992aec7b0460765fac9409c03ed3df7d47
Billy Wolfington <man_of_magik903@yahoo.com>
2014-08-22 00:41:17 -0400

    Building on iframes
    Moved the javascript for loading and interacting with the page to
    default.js.  Added a container that holds information on what tabs
    should display on the sidebar and what iframes should be created.  If
    an iframe does not exist for a tab, the “Home Page” tab appears.

---
+++ js/default.js
@@ -0,0 +1,88 @@
+$(function() {
+    var _sidebarTabs = [
+        {
+            tabName: "EECS 183",
+            iframeSource: "sample.html"
+        },
+        {
+            tabName: "Syllabus",
+            iframeSource: "http://umich.edu/~eecs183/syllabus"
+        },
+        {
+            tabName: "Calendar",
+            iframeSource: "http://umich.edu/~eecs183/calendar"
+        },
+        {
+            tabName: "Projects",
+            iframeSource: ""
+        },
+        {
+            tabName: "Lectures",
+            iframeSource: ""
+        },
+        {
+            tabName: "Resources",
+            iframeSource: ""
+        },
+        {
+            tabName: "Staff",
+            iframeSource: ""
+        },
+        {
+            tabName: "Contact",
+            iframeSource: ""
+        },
+    ];
+
+    $("body").height(window.innerHeight);
+
+    function createElement(elementType) {
+        return $(document.createElement(elementType));
+    }
+
+
+    function createSidebarTabsAnd_iFrames() {
+        for (var i in _sidebarTabs) {
+            var currentTab = _sidebarTabs[i];
+            if (currentTab.tabName)
+                appendSidebarItem(currentTab.tabName, i);
+            if (currentTab.iframeSource)
+                append_iFrame(currentTab.iframeSource, i);
+        }
+    }
+    function appendSidebarItem(sidebarName, index) {
+        var listItem = createElement("li").attr("tabIndex", index);
+        listItem.append(createElement("span").text(sidebarName));
+        if (index == "0")
+            listItem.addClass("sidebar-brand active");
+
+        $("#sidebar-wrapper ul").append(listItem);
+    }
+    function append_iFrame(iframeSource, index) {
+        var iFrame = createElement("iframe").attr("frameborder", "0").addClass("pageContent row-fluid");
+        iFrame.attr("tabIndex", index).attr("src", iframeSource);
+
+        if (index != "0")
+            iFrame.addClass("inactiveFrame");
+
+        $("#iFrameContainer").append(iFrame);
+    }
+    createSidebarTabsAnd_iFrames();
+
+    
+
+    $("#sidebar-wrapper li").click(function() {
+        var listItem = $(this),
+            iFrameContainer = $("#iFrameContainer"),
+            iframeIndex = listItem.attr("tabIndex");
+        iFrameContainer.find("iframe").addClass("inactiveFrame");
+        $("#sidebar-wrapper li").removeClass("active");
+        
+        var selectedIframe = iFrameContainer.find("iframe[tabIndex='" + iframeIndex + "']");
+        if (!selectedIframe.length) {
+            selectedIframe = iFrameContainer.find("iframe[tabIndex='0']");
+        }
+        selectedIframe.removeClass("inactiveFrame");
+        listItem.addClass("active")
+    });
+});
