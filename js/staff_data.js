2a4e3e281b0e9e14897d9d0cc4e05bde8a409625
Billy Wolfington <man_of_magik903@yahoo.com>
2014-09-02 23:55:11 -0400

    More Staff data added

--- js/staff_data.js
+++ js/staff_data.js
@@ -238,7 +238,7 @@ var _instructors = {
         new Instructor("Grace", "Kendall", false, [], [], "W'16", "", "", "", [], []),
         new Instructor("Jessica", "Wu", false, ["Computer Science"], ["Japanese", "Physics"], "W'16", "", "jesswu@umich.edu", "", [], []),
         new Instructor("Lisa", "Dion", false, ["Computer Science"], [], "2nd year Ph.D.", "", "", "", [], []),
-        new Instructor("Madeline", "Enders", false, [], [], "", "", "", "", [], []),
+        new Instructor("Madeline", "Enders", false, ["Computer Science", "Cello Performance"], [], "W'17", "", "endremad@umich.edu", "", [], []),
         new Instructor("Maxim", "Aleksa", true, ["Computer Science", "Romance Languages and Literatures",], ["Master of the Universe"], "W'17", "", "maximal@umich.edu", "umich.edu/~maximal", [3], [5, 2, 3]),
         new Instructor("Michael", "Vianueva", false, ["Computer Science", "History"], ["German"], "W'15", "", "vianuevm@umich.edu", "github.com/vianuevm", [], []),
         new Instructor("Reed", "Coke", false, ["Computer Science", "Linguistics"], [], "2nd year Ph.D.", "", "reedcoke@umich.edu", "", [], []),
